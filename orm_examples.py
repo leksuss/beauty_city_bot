c4cd9b23cebd87cc0f74f04970732936e7ea70c8
Andrey Alekseev <leksus@leksus.info>
2023-05-26 19:26:43 +0300

    add more examples

--- orm_examples.py
+++ orm_examples.py
@@ -22,21 +22,27 @@ masters = Master.objects.all()
 ####
 # Список свободных слотов для мастера на конкретный день
 ####
+# клиент выбрал мастера
+master_id = 1
+master = Master.objects.get(pk=master_id)
 # клиент выбрал дату
 date_string = '2023-05-26'
 # конвертируем в datetime объект
 date_object = datetime.strptime(date_string, '%Y-%m-%d').date()
-empty_slots_for_master = Master.get_empty_slots(date_object)
+empty_slots_for_master = master.get_empty_slots(date_object)
 
 
 ####
 # Список свободных слотов для процедуры на конкретный день
 ####
+# клиент выбрал мастера
+procedure_id = 1
+procedure = Procedure.objects.get(pk=procedure_id)
 # клиент выбрал дату
 date_string = '2023-05-26'
 # конвертируем в datetime объект
 date_object = datetime.strptime(date_string, '%Y-%m-%d').date()
-empty_slots_for_procedure = Procedure.get_empty_slots(date_object)
+empty_slots_for_procedure = procedure.get_empty_slots(date_object)
 
 
 ####
@@ -60,7 +66,7 @@ date_object = datetime.strptime(date_string, '%Y-%m-%d').date()
 # клиент выбрал id слота
 time_slot_id = 2
 time_slot = Time.objects.get(pk=time_slot_id)
-
+# добавляем запись на определенную процедуру к определенному мастеру
 appointment_with_master = Appointment.add_with_master(master, procedure, client, date_object, time_slot)
 
 
@@ -81,5 +87,7 @@ date_object = datetime.strptime(date_string, '%Y-%m-%d').date()
 # клиент выбрал id слота
 time_slot_id = 2
 time_slot = Time.objects.get(pk=time_slot_id)
-
+# добавляем запись на определенную процедуру к любому свободному мастеру
+# при этом у нас не может быть, что на это время нет свободных мастеров,
+# т.к. мы до этого выбрали свободный слот, где точно есть запись.
 appointment_with_procedure = Appointment.add_with_procedure(procedure, client, date_object, time_slot)
